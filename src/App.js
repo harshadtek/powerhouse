1d6ffb044ab4ab42128b8c2a34311e5500694743
Harshad Ahemad Raj K <hak@BLR.ALLEGISINDIA.COM>
2022-04-22 14:00:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Display a Submit Button</h1>
+
+<form action="/action_page.php">
+  <label for="fname">First name: </label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Last name: </label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+        <button onclick="myFunction()">ENTER</button>
         <a
           className="App-link"
           href="https://reactjs.org"
